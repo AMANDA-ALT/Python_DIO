fa6467c1854d57335ead120136e0616a02dd701b
Amanda <amandabaratadev@gmail.com>
2023-09-03 19:09:32 +0100

    Print and Input Functions

---
+++ print_input.py
@@ -0,0 +1,6 @@
+name = input('Inform your name: ')
+age = input('How old are you? ')
+
+print(name, age, end = ' ... \n')
+print(name,age, sep = ' -  ')
+print(name, age, sep = '#', end = ' ... \n ')
