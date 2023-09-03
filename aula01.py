c36eef69127fbd3950ea4744219b9133ac389328
Amanda <amandabaratadev@gmail.com>
2023-09-03 15:52:05 +0100

    First Class

---
+++ aula01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+print(10+11+1000)
+print(1.5+1+0.5)
+print(True)
+print(False)
+print('Python')
+int()
+print('Welcome to python3')
+age, name = (40,’Amanda')
+print(f'My name is {name} and i am {age}years old')
