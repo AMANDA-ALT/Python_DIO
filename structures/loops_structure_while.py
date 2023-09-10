e2f4cc3a06b6634bdcf922bb8d40b1ec7e809b42
Amanda <amandabaratadev@gmail.com>
2023-09-10 19:11:00 +0100

    loops while

---
+++ structures/loops_structure_while.py
@@ -0,0 +1,11 @@
+option = -1
+
+while option !=0:
+    option = int(input('[1] Withdraw \n [2] Balance \n [0] Exit \n: '))
+
+    if option == 1:
+        print('Sacando...')
+    elif option == 2:
+        print('Showing balance...')  
+else:
+        print('Tks for using our services!')      
