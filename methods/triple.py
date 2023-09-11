28574e6a1cc3c28b32433bdb0bbd7dac50515b2e
Amanda <amandabaratadev@gmail.com>
2023-09-11 15:19:16 +0100

    multiple lines

---
+++ methods/triple.py
@@ -0,0 +1,23 @@
+name = 'Amanda'
+
+message = f'''
+   Hello, my name is {name},
+I'm learning Python.
+      This message has different identations.
+      '''
+
+print(message)
+
+
+print('''
+   ========== MENU ========
+1 - Deposit
+2 - Withdraw
+0 - Exit
+      
+    ========================
+      
+      Tks for using our system!
+
+      ''')
+
